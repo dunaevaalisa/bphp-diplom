d2c7b814ec8b337267167a7aa67fc4399dfee4ec
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-09-21 22:48:42 +0300

    Create main.php

---
+++ pages/main.php
@@ -0,0 +1,8 @@
+
+    <main class="content-wrapper">
+        <section class="content">
+            <ul class="projects-list <?= $_SESSION['role']?>">
+            </ul>
+        </section>
+    </main>
+</div>
