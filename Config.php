16b5bc367dab7438412210014115c4daccf8d81f
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-09-21 23:59:22 +0300

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Config class
+ */
+class Config
+{
+    /**
+     * @var 'files/database/' DATABASE_PATH
+     * @var 'files/database/projects/' PROJECTS_PATH
+     */
+    const DATABASE_PATH = 'files/database/';
+    const PROJECTS_PATH = 'files/database/projects/';
+}
